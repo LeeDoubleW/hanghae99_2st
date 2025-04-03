34fbbe96321f9d6c30e38481af01cbae5223b8f7
LeeDoubleW <kalimdors@naver.com>
2025-04-04 04:19:46 +0900

    잔액 API

---
+++ src/main/java/kr/hhplus/be/server/controller/UserController.java
@@ -0,0 +1,55 @@
+package kr.hhplus.be.server.controller;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/user")
+@Tag(name = "User", description = "잔액 충전 및 조회 API")
+public class UserController {
+	
+	@GetMapping("/{userId}")
+    @Operation(
+            summary = "잔액 조회",
+            description = "유저의 잔액을 조회합니다."
+    )
+    public ResponseEntity<Map<String, Object>> getBalance(
+            @Parameter(description = "유저ID", example = "1")
+            @PathVariable("userId") Long userId
+    ) {
+        int balance = 10000;
+
+        return ResponseEntity.ok(Map.of(
+                "userId", userId,
+                "balance", balance,
+                "message", "잔액 조회 성공"
+        ));
+    }
+	
+	@GetMapping("/{userId}/charge")
+    @Operation(
+            summary = "잔액 충전",
+            description = "유저의 잔액을 충전합니다."
+    )
+    public ResponseEntity<Map<String, Object>> getBalance(
+            @Parameter(description = "유저ID", example = "1")
+            @PathVariable("userId") Long userId,
+            @RequestBody
+            @Parameter(description = "충전 금액 (JSON 형태)", required = true, example = "{\"amount\":5000}")
+            Map<String, Object> body
+    ) {
+        int balance = 10000;
+
+        return ResponseEntity.ok(Map.of(
+                "userId", userId,
+                "balance", balance,
+                "message", "잔액 조회 성공"
+        ));
+    }
+}
---
+++ src/main/java/kr/hhplus/be/server/dto/balance/BalanceResponse.java
@@ -0,0 +1,3 @@
+package kr.hhplus.be.server.dto.balance;
+
+public record BalanceResponse (long userId, long amount) {}
