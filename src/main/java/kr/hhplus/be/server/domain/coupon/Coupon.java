dea8774e42e5a6d9f95b20a7aa4ca6e840f10b1e
LeeDoubleW <kalimdors@naver.com>
2025-04-11 09:28:22 +0900

    임시 커밋

--- src/main/java/kr/hhplus/be/server/domain/coupon/Coupon.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/Coupon.java
@@ -1,5 +1,19 @@
 package kr.hhplus.be.server.domain.coupon;
 
-public class Coupon {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
+@Entity(name = "coupon")
+@Getter
+@Accessors(fluent = true)
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Coupon {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 }
